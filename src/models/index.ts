6803181d27ec879933b1dd325a6d403e358cc628
Uzochukwu Onuegbu <uzochukwu.onuegbu25@gmail.com>
2023-07-16 21:23:59 +0200

    added pre-commit hooks using husky

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,24 +5,6 @@ import Category from './categories.model';
 import Collection from './collections.model';
 import CollectionCategories from './collectionsCategories.model';
 
-// Constructors.hasMany(Teams, {
-//   foreignKey: "constructorId",
-//   as: "teamsList"
-// });
-// Teams.belongsTo(Constructors, {
-//   foreignKey: "constructorId",
-//   as: "constructor"
-// });
-
-// Championships.hasMany(Teams, {
-//   foreignKey: "championshipId",
-//   as: "teamsList"
-// });
-// Teams.belongsTo(Championships, {
-//   foreignKey: "championshipId",
-//   as: "championship"
-// });
-
 export {
     Asset,
     Auth,
